cc6d27fd48aef302e35f8e4c36ece7b68cdbd019
p4matty <mattycovar@gmail.com>
2026-03-26 09:21:24 +0800

    Ver 4.1
    Updated GameOver logic

--- CCDSTRU_MP_ver4.c
+++ CCDSTRU_MP_ver4.c
@@ -346,18 +346,19 @@ void nextplayermove(int over, int pos, int good, int start,
         
 }
 
-void GameOver(int over, int R[3][3], int B[3][3]){
+void GameOver(int *over, int R[3][3], int B[3][3]){
 
     int i, j;
-    int countR = 0;
-    int countB = 0;
+    int Ramt = 0;
+    int Bamt = 0;
+	int Final = 0;
 //just tweaked gameover so that it doesn't force game to end and checks is over is 1. also put the printf inside the if statement.
 
 	if(over == 1){
 		for(i = 0; i < 3; i++){
 			for(j = 0; j < 3; j++){
 				if(R[i][j] != 0){
-					countR++;
+					Ramt++;
 				}
 			}
 		}
@@ -365,10 +366,16 @@ void GameOver(int over, int R[3][3], int B[3][3]){
 		for(i = 0; i < 3; i++){
 			for(j = 0; j < 3; j++){
 				if(B[i][j] != 0){
-					countB++;
+					Bamt++;
 				}
 			}
 		}
+
+		Final = 9 - (Ramt + Bamt);
+
+		if(Final == 3 || val >= 20 || (!start && ((Ramt > 0 && Bamt == 0) || (Ramt == 0 && Bamt > 0)))){
+			*over = 1
+				}
 
     	if(countR > countB){
         	printf("Player 1 (R) wins!");
